cc83289a0e111be8bdac7b3ad3cf8a7c573d41de
Marta Kolodziejczak <marta@martas-mbp.intern>
2019-05-22 11:28:27 +0200

    input form

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from '../styles/logo.svg';
 import '../styles/App.scss';
+import IntroForm from './IntroForm';
 
 function App() {
   return (
     <div className="App">
-      
+      <IntroForm/>
     </div>
   );
 }
