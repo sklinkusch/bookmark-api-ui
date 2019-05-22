b97efc5d636b18d97c4fdb5cbc53befc8ef48f13
Marta Kolodziejczak <marta@martas-mbp.intern>
2019-05-22 11:10:11 +0200

    cleaned up app.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,7 @@ import '../styles/App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://bookmark-api.fbw-11.now.sh/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button type="button" className="btn btn-primary"><i className="fab fa-chrome"></i> Bookmark API v1.0</button>
-
-        </a>
-      </header>
+      
     </div>
   );
 }
